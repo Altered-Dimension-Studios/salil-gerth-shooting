9a4b53dd4b9991197467298b9fff2871ba67c959
Caciuleanu Ciprian <caciuleanu.ciprian@gmail.com>
2024-11-16 19:25:18 +0200

    car and bullet

--- entities/car/car.gd
+++ entities/car/car.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const bulletPath = preload('res://entities/bullet/bullet.tscn')
+
 
 const SPEED: float = 300.0
 const MIN_CLAMP_VECTOR: Vector2 = Vector2(100, 0)
@@ -16,9 +18,21 @@ func _process(delta: float) -> void:
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+	if Input.is_action_just_pressed("shoot"):
+		print('shoot')
+		print($Node2D/Marker2D.global_transform)
+		shoot()
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 	
 	position += velocity * delta
 	position = position.clamp(MIN_CLAMP_VECTOR, MAX_CLAMP_VECTOR)
+	$Node2D.look_at(get_global_mouse_position())
+
+func shoot():
+	var bullet = bulletPath.instantiate()
+	
+	get_parent().add_child(bullet)
+	bullet.transform = $Node2D/Marker2D.global_transform
+	
